be31f173894a613705ca63450ba26f7d758e4394
naberin <aberinnj@gmail.com>
2021-09-27 23:42:29 -0500

    Added Redis import and requirement

--- votes/app.py
+++ votes/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request
 from controllers.mainController import MainController
+from config.constants import Constants
+
+import redis
 
 app = Flask(__name__)
+rds = redis.Redis(host=Constants.REDIS_HOST, port=Constants.REDIS_PORT, db=0)
 
 
 @app.route("/api/health")
---
+++ votes/config/constants.py
@@ -0,0 +1,3 @@
+class Constants:
+    REDIS_PORT = 6379
+    REDIS_HOST = 'localhost'
