ff740e92e53f0df45a4bd701e8afecc9df660103
naberin <aberinnj@gmail.com>
2021-09-29 18:06:49 -0500

    Added POST and GET games

--- games/app.py
+++ games/app.py
@@ -15,15 +15,35 @@ celery = CeleryApp(app).get()
 rds = redis.Redis(host='localhost', port=6379, db=0)
 rds.set("games:id", 999)
 
+
 @app.route("/api/health")
 def health():
     return jsonify({"status": "UP"}), 200
+
+
+@app.route("/api/games", methods=["POST"])
+def post_games():
+
+    gid = _get_next_id()
+    game_id = f"G{gid}"
+
+    rds.lpush('games', game_id)
+
+@app.route("/api/games", methods=["GET"])
+def get_games():
+    number_of_games = rds.llen('games')
+    list_of_games = [g.decode('UTF-8') for g in rds.lrange('games', 0, number_of_games)]
+
+    return jsonify({"games": list_of_games, "count": number_of_games}), 200
 
 
 @celery.on_after_configure.connect()
 def set_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(10.0, del_stale_games.s(), name='Delete Stale Games every 10 seconds')
 
+
+def _get_next_id():
+    return rds.incr("games:id")
 
 
 @celery.task()
