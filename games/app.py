94fe68668a7b845b07b78e558424f037f9bb44ed
naberin <aberinnj@gmail.com>
2021-09-29 18:06:24 -0500

    Added Periodic Task deleting stale games in the list

--- games/app.py
+++ games/app.py
@@ -1,8 +1,35 @@
+import sys
+import redis
+
 from flask import Flask, jsonify
+from services.celery import CeleryApp
+
+
 app = Flask(__name__)
+app.config.update(
+    CELERY_BROKER_URL='redis://localhost:6379/0',
+    CELERY_RESULT_BACKEND='redis://localhost:6379/0'
+)
+celery = CeleryApp(app).get()
+
+rds = redis.Redis(host='localhost', port=6379, db=0)
+rds.set("games:id", 999)
+
 @app.route("/api/health")
 def health():
     return jsonify({"status": "UP"}), 200
+
+
+@celery.on_after_configure.connect()
+def set_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(10.0, del_stale_games.s(), name='Delete Stale Games every 10 seconds')
+
+
+
+@celery.task()
+def del_stale_games():
+    return rds.rpop("games")
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',
